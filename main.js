3ce93dc485a1e982abe7c39c17d598b7657230a4
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-09-27 16:42:09 +0200

    adding changes to reproduce the issue

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
 function createWindow() {
-  // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,44 +10,28 @@ function createWindow() {
       nodeIntegration: true,
     }
   })
-
-  // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
-  // Open the DevTools.
   mainWindow.webContents.openDevTools()
 
-  // I added
-  console.log('before set timeout');
-  setTimeout(() => {
-    let anotherWindow = new BrowserWindow({
-      width: 800,
-      height: 600
-    });
-    anotherWindow.loadFile('index.html');
-  }, 5000)
-  // I added
+  // setTimeout(() => {
+  //   let anotherWindow = new BrowserWindow({
+  //     width: 800,
+  //     height: 600
+  //   });
+  //   anotherWindow.loadFile('index2.html')
+  //   anotherWindow.webContents.openDevTools();
+  //   console.log('anotherWindow : ', anotherWindow)
+  // }, 5000)
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,12 +1,3 @@
-/**
- * This file is loaded via the <script> tag in the index.html file and will
- * be executed in the renderer process for that window. No Node.js APIs are
- * available in this process because `nodeIntegration` is turned off and
- * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
- * to expose Node.js functionality from the main process.
- */
-
-console.log('before my changes')
 const IS_EDIT_CONTEXT_SUPPORTED = 'EditContext' in window;
 console.log('IS_EDIT_CONTEXT_SUPPORTED : ', IS_EDIT_CONTEXT_SUPPORTED);
 
@@ -14,13 +5,23 @@ const editorEl = document.getElementById("html-editor");
 editorEl.style.height = '200px';
 editorEl.style.width = '100%';
 
-if (!IS_EDIT_CONTEXT_SUPPORTED) {
-    editorEl.textContent = "Sorry, your browser doesn't support the EditContext API. This demo will not work.";
+if (IS_EDIT_CONTEXT_SUPPORTED) {
+    const editContext = new EditContext();
+    editorEl.editContext = editContext;
+
+    editContext.addEventListener("textupdate", e => {
+        console.log('textupdate : ', e);
+    });
+    editorEl.addEventListener('keydown', e => {
+        console.log('keydown : ', e);
+    });
+
+    setTimeout(() => {
+        const body = window.document.body;
+        const newWindow = window.open();
+        newWindow?.window?.document.body.append(body);
+        console.log('body : ', body);
+        console.log('newWindow : ', newWindow);
+    }, 5000)
 }
 
-const editContext = new EditContext();
-editorEl.editContext = editContext;
-
-editContext.addEventListener("textupdate", e => {
-    console.log('textupdate e : ', e);
-});
